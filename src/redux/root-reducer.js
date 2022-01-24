7145b06c1f48e24662e7ade02822f886caacf008
Al-amin <alaminazad240@gmail.com>
2022-01-25 00:07:18 +0600

    redux-persist added on cart

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,17 @@
 import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import cartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+  }
+const rootReducer= combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig,rootReducer)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-const middleWares =[logger];
+const middleWares = [logger];
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
-export default store;
+const persistor = persistStore(store);
+// eslint-disable-next-line import/no-anonymous-default-export
+export { store, persistor };
